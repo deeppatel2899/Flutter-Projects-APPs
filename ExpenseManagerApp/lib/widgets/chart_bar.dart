05109c3966a839c2c4d05f71d01bcf42e83ca963
Deep Patel <61212533+deeppatel2899@users.noreply.github.com>
2021-07-02 13:20:19 -0700

    Create chart_bar.dart

---
+++ ExpenseManagerApp/lib/widgets/chart_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Chartbar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPct;
+
+  Chartbar(
+    this.label,
+    this.spendingAmount,
+    this.spendingPct,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text("\$${(spendingAmount.toStringAsFixed(0))}"),
+          ),
+        ),
+        SizedBox(height: 4),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                ),
+                color: Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(10),
+              )),
+              FractionallySizedBox(
+                heightFactor: spendingPct,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(label),
+      ],
+    );
+  }
+}
